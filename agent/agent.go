88dc7e204f0afba3f648a4ad229844b10e9f4da2
chriskaliX <chriskali@163.com>
2021-09-14 23:06:04 +0800

    architecture changing & thinking

--- agent/agent.go
+++ agent/agent.go
@@ -3,12 +3,29 @@ package main
 import (
 	"time"
 
-	// "github.com/chriskaliX/HIDS-Linux/agent/collector"
-	"github.com/chriskaliX/HIDS-Linux/agent/transport/domain"
+	"github.com/chriskaliX/HIDS-Linux/agent/collector"
+	"github.com/chriskaliX/HIDS-Linux/agent/global"
 )
 
 func main() {
-	go domain.ServerRun()
 	time.Sleep(1 * time.Second)
-	// collector.Run()
+	collector.Run()
+
+	buf := make([]map[string]string, 0, 100)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case rd := <-global.UploadChannel:
+			buf = append(buf, rd)
+		case <-ticker.C:
+			if len(buf) != 0 {
+				err := client.Send(buf)
+				buf = buf[:0]
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
 }
