ab5b6bca498923a0d52da0145f562a4961450a82
chriskaliX <chriskali@163.com>
2021-09-21 22:40:41 +0800

    ebpf pre-learning

--- agent/collector/ebpf.go
+++ agent/collector/ebpf.go
@@ -5,3 +5,4 @@ package collector
 // 因为 ebpf 有 DAG check & Execution Simulation Check
 // 并且限制了 limit 4096, 较为安全
 // 还没看完, 重点关注 kprobe, 参考一下 osquery (新版本已经引入 ebpf 了)
+// osquery 的 ebpf 相关地址 https://github.com/osquery/osquery/tree/d2be385d71f401c85872f00d479df8f499164c5a/osquery/events/linux/bpf
