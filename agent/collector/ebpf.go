76d5a22a313e85a99fe9a5056761360403e7c347
chriskaliX <chriskali@163.com>
2021-09-23 23:19:55 +0800

    starting ebpf

--- agent/collector/ebpf.go
+++ agent/collector/ebpf.go
@@ -6,3 +6,4 @@ package collector
 // 并且限制了 limit 4096, 较为安全
 // 还没看完, 重点关注 kprobe, 参考一下 osquery (新版本已经引入 ebpf 了)
 // osquery 的 ebpf 相关地址 https://github.com/osquery/osquery/tree/d2be385d71f401c85872f00d479df8f499164c5a/osquery/events/linux/bpf
+
---
+++ agent/collector/ebpf/ebpf.c
@@ -0,0 +1,1 @@
+#include <linux/bpf.h>
