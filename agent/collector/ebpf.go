56721da9b03db5de44f25682b394bcda31d9d1c0
chriskaliX <root@localhost.localdomain>
2021-10-01 23:39:15 +0800

    test one

--- agent/collector/ebpf.go
+++ agent/collector/ebpf.go
@@ -13,11 +13,7 @@ import (
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-12 KProbeExample ./ebpf/ebpf.c -- -nostdinc -I/root/projects/Hades/agent/collector/ebpf/headers/
 
-// ebpf 的采集
-// 大致逻辑为: 写一段代码让内核 load
-// 因为 ebpf 有 DAG check & Execution Simulation Check
-// 并且限制了 limit 4096, 较为安全
-// 还没看完, 重点关注 kprobe, 参考一下 osquery (新版本已经引入 ebpf 了)
+// ebpf 的采集 - test
 // osquery 的 ebpf 相关地址 https://github.com/osquery/osquery/tree/d2be385d71f401c85872f00d479df8f499164c5a/osquery/events/linux/bpf
 
 const mapKey uint32 = 0
