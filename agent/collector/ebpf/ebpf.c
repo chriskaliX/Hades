fa9ba5e2844c85158d204b6518bd9b0f94ddac0b
chriskaliX <chriskali@163.com>
2021-10-03 01:38:31 +0800

    ebpf read comm is fine, working on argv

--- agent/collector/ebpf/ebpf.c
+++ agent/collector/ebpf/ebpf.c
@@ -6,12 +6,6 @@
     https://github.com/iovisor/bcc/blob/master/docs/reference_guide.md
 */
 
-/* 
-* 基本上很少看到 ebpf 如何写的
-* 所以基本就靠直接啃, 不懂的就搜索
-* C 的基础属于幼儿园水平, 但是没关系, 慢慢扣、多学习
-*/
-
 /*
     issue 1: 碰到 LLVM 的问题
     https://github.com/cilium/ebpf/issues/43
@@ -23,7 +17,8 @@
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 // 截取长度最大值
-#define TASK_COMM_LEN 16
+#define TASK_COMM_LEN 256
+#define ARGV_LEN 256
 
 // execve struct
 struct execve {
@@ -32,6 +27,7 @@ struct execve {
     u32 gid;
     u32 ppid;
     char comm[TASK_COMM_LEN];
+    char argv[ARGV_LEN];
 };
 
 // 定义返回通信的 map array?
@@ -42,23 +38,43 @@ struct bpf_map_def SEC("maps/execve_events") execve_events = {
         .max_entries = 1024,
 };
 
+// 参考 https://github.com/iovisor/bcc/blob/e83019bdf6c400b589e69c7d18092e38088f89a8/tools/execsnoop.py
 SEC("kprobe/sys_execve")
 int bpf_sys_execve(struct pt_regs *ctx)
 {
     // bpf 的默认结构体, 查看 bpf_helper_defs.h 前面申明的 struct, 获取当前 attach 信息
+    // task_struct 是一个普遍的结构体, 详细定义看 vmlinux.h (从 common.h 替换了)
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 
-    // 创建 execve 结构体
-    struct execve execve_data = {
-        .pid = bpf_get_current_pid_tgid() >> 32,
-        .uid = bpf_get_current_uid_gid() >> 32,
-        .gid = bpf_get_current_uid_gid(),
-        .ppid = task->real_parent->tgid // 这个有bug, 在一些内核版本下会直接返回 0
-    };
+    // execve_data
+    // struct execve execve_data = {
+    //     .pid = bpf_get_current_pid_tgid() >> 32,
+    //     .uid = bpf_get_current_uid_gid() >> 32,
+    //     .gid = bpf_get_current_uid_gid(),
+    //     .ppid = task->real_parent->tgid, // 这个有bug, 在一些内核版本下会直接返回 0, 再搜一下
+    // };
+    struct execve execve_data;
 
+    // https://github.com/iovisor/bcc/issues/2623
+    __builtin_memset(&execve_data, 0, sizeof(execve_data));
+    execve_data.pid = bpf_get_current_pid_tgid() >> 32;
+    execve_data.uid = bpf_get_current_uid_gid() >> 32;
+    execve_data.gid = bpf_get_current_uid_gid();
+    execve_data.ppid = task->real_parent->tgid >> 32;
 
     bpf_get_current_comm(&execve_data.comm, sizeof(execve_data.comm));
+
+    // const char *argp = NULL;
+    // bpf_probe_read_user(&argp, sizeof(argp), ptr);
+
+    // https://zhidao.baidu.com/question/684624210709860732.html
+    // const char __user *const __user *__argv;
+    // const char *argp = NULL;
+    // bpf_probe_read_user(execve_data.argv, sizeof(&execve_data.argv), (void *)&__argv);
+
     bpf_perf_event_output(ctx, &execve_events, BPF_F_CURRENT_CPU, &execve_data, sizeof(execve_data));
+    // handle truncated argument list
+    // char ellipsis[] = "...";
 
     return 0;
 }
