eabe5d915fa37df46d48a3d1f3daaf90ba955d05
chriskaliX <chriskali@163.com>
2021-10-04 22:36:58 +0800

    ebpf demo done

--- agent/collector/ebpf/ebpf.c
+++ agent/collector/ebpf/ebpf.c
@@ -1,9 +1,14 @@
-// #include "common.h"
+/*
+    都从libbpf里搬运, CO-RE
+*/
 #include "vmlinux.h"
 #include "bpf_helpers.h"
+#include "bpf_tracing.h" // tracing, 来读取上下文
+
 /*
     记录一些reference
     https://github.com/iovisor/bcc/blob/master/docs/reference_guide.md
+    https://github.com/andrewkroh/go-ebpf/blob/db8c37d734c59d955b679c721e12a342e3b5d93c/exec/bpf/exec.c
 */
 
 /*
@@ -17,15 +22,16 @@
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 // 截取长度最大值
-#define TASK_COMM_LEN 256
+#define TASK_COMM_LEN 16
 #define ARGV_LEN 256
+#define __user
 
 // execve struct
-struct execve {
+struct execve_t {
     u32 pid;
     u32 uid;
     u32 gid;
-    u32 ppid;
+    int ppid;
     char comm[TASK_COMM_LEN];
     char argv[ARGV_LEN];
 };
@@ -42,39 +48,59 @@ struct bpf_map_def SEC("maps/execve_events") execve_events = {
 SEC("kprobe/sys_execve")
 int bpf_sys_execve(struct pt_regs *ctx)
 {
-    // bpf 的默认结构体, 查看 bpf_helper_defs.h 前面申明的 struct, 获取当前 attach 信息
     // task_struct 是一个普遍的结构体, 详细定义看 vmlinux.h (从 common.h 替换了)
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 
     // execve_data
-    // struct execve execve_data = {
-    //     .pid = bpf_get_current_pid_tgid() >> 32,
-    //     .uid = bpf_get_current_uid_gid() >> 32,
-    //     .gid = bpf_get_current_uid_gid(),
-    //     .ppid = task->real_parent->tgid, // 这个有bug, 在一些内核版本下会直接返回 0, 再搜一下
-    // };
-    struct execve execve_data;
-
+    struct execve_t execve_data;
     // https://github.com/iovisor/bcc/issues/2623
     __builtin_memset(&execve_data, 0, sizeof(execve_data));
     execve_data.pid = bpf_get_current_pid_tgid() >> 32;
     execve_data.uid = bpf_get_current_uid_gid() >> 32;
     execve_data.gid = bpf_get_current_uid_gid();
+    // 获取 ppid 的在某些内核版本下会为 0, 需要 fallback
     execve_data.ppid = task->real_parent->tgid >> 32;
-
+    // 获取 comm
     bpf_get_current_comm(&execve_data.comm, sizeof(execve_data.comm));
 
-    // const char *argp = NULL;
-    // bpf_probe_read_user(&argp, sizeof(argp), ptr);
-
     // https://zhidao.baidu.com/question/684624210709860732.html
-    // const char __user *const __user *__argv;
-    // const char *argp = NULL;
-    // bpf_probe_read_user(execve_data.argv, sizeof(&execve_data.argv), (void *)&__argv);
+    /*
+        从上下文中获取信息, 第二个是 argv, 参考
+        https://blog.csdn.net/rikeyone/article/details/114586276
+        这个 __user 标识一下是用户态的
+    */
 
-    bpf_perf_event_output(ctx, &execve_events, BPF_F_CURRENT_CPU, &execve_data, sizeof(execve_data));
-    // handle truncated argument list
+    const char __user *const __user *argv = (void *)PT_REGS_PARM2(ctx);
+    char *argp = NULL;
+    // bpf_probe_read_user(&argp, sizeof(argp), (void *)argv);
+    bpf_probe_read_user_str(&argp, sizeof(argp), (void *)argv);
+    if (argp) {
+        bpf_probe_read(execve_data.argv, sizeof(execve_data.argv), argp);
+    }
+    
+    // char ellipse[] = "...";
+    // bpf_probe_read(execve_data.argv, sizeof(execve_data.argv), (void*)ellipse);
+    // bpf_perf_event_output(ctx, &execve_events, cpu, &execve_data, sizeof(execve_data));
+
+    // return 0;
+
+    // test2:
+    // for (int i = 1; i < ARGV_LEN; i++) {
+    //     const char *argp = NULL;
+    //     bpf_probe_read_user(&argp, sizeof(argp), (void *)&argv[i]);
+    //     if (argp) {
+    //         bpf_probe_read_user(execve_data.argv, sizeof(&execve_data.argv), argp);
+    //         continue;
+    //     }
+    //     goto out;
+    // }
+
+
+    // const char *argp = NULL;
     // char ellipsis[] = "...";
 
+    // bpf_perf_event_output(ctx, &execve_events, BPF_F_CURRENT_CPU, &execve_data, sizeof(execve_data));
+// out:
+    bpf_perf_event_output(ctx, &execve_events, BPF_F_CURRENT_CPU, &execve_data, sizeof(execve_data));
     return 0;
 }
