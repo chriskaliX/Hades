1ee606ea0ff972426a01d922d3696e2871471268
chriskaliX <chriskali@163.com>
2021-11-10 23:45:14 +0800

    drop test

--- agent/collector/ebpf/tracepoint_execve.go
+++ agent/collector/ebpf/tracepoint_execve.go
@@ -29,6 +29,11 @@ type exec_data_t struct {
 	Arg_size uint32
 }
 
+// 今天继续读 perf_events, 发现这么写是有一些问题的, 加到 todo 里后续改进
+// TODO:
+// 1. 乱序问题, 由于现在开发的机器是腾讯云的单CPU机器, 所以按照这么写永远是有顺序的。周末开一台多核的, 把代码拉上去 run 一下作为验证, 并加上 fork 程序?
+// 2. 看 cilium/ebpf/perf/reader.go 实现, 看一下 perCPU ringbuf 大小定义(可能不在这里) 来解决 ring buffer full 导致的 drop 问题
+
 func Tracepoint3() {
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal(err)
@@ -46,7 +51,8 @@ func Tracepoint3() {
 	}
 	defer kp.Close()
 
-	rd, err := perf.NewReader(objs.PerfEvents, os.Getpagesize())
+	// test for todo-2
+	rd, err := perf.NewReader(objs.PerfEvents, 2*os.Getpagesize())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -88,7 +94,7 @@ func Tracepoint3() {
 
 		} else {
 			// fmt.Printf("[INFO] pid: %d, comm: %s, argv: %s\n", event.Pid, string(event.Comm[:]), strings.Trim(args, " "))
-			count = count+1
+			count = count + 1
 			pid = event.Pid
 			args = string(event.Args[:]) + " "
 			// fmt.Println(count)
