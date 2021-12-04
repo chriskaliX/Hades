96b0fa9032b9d136fdd1da4a0d850c1cde041a86
chriskaliX <chriskali@163.com>
2021-12-04 23:08:53 +0800

    开始看一下非core

--- agent/collector/ebpf/headers/common.h
+++ agent/collector/ebpf/headers/common.h
@@ -69,39 +69,39 @@ enum {
 #define BPF_F_CURRENT_CPU BPF_F_INDEX_MASK
 
 #define PT_REGS_RC(x) ((x)->rax)
-struct pt_regs {
-/*
- * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
- * unless syscall needs a complete, fully filled "struct pt_regs".
- */
-	unsigned long r15;
-	unsigned long r14;
-	unsigned long r13;
-	unsigned long r12;
-	unsigned long rbp;
-	unsigned long rbx;
-/* These regs are callee-clobbered. Always saved on kernel entry. */
-	unsigned long r11;
-	unsigned long r10;
-	unsigned long r9;
-	unsigned long r8;
-	unsigned long rax;
-	unsigned long rcx;
-	unsigned long rdx;
-	unsigned long rsi;
-	unsigned long rdi;
-/*
- * On syscall entry, this is syscall#. On CPU exception, this is error code.
- * On hw interrupt, it's IRQ number:
- */
-	unsigned long orig_rax;
-/* Return frame for iretq */
-	unsigned long rip;
-	unsigned long cs;
-	unsigned long eflags;
-	unsigned long rsp;
-	unsigned long ss;
-/* top of stack page */
-};
+// struct pt_regs {
+// /*
+//  * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
+//  * unless syscall needs a complete, fully filled "struct pt_regs".
+//  */
+// 	unsigned long r15;
+// 	unsigned long r14;
+// 	unsigned long r13;
+// 	unsigned long r12;
+// 	unsigned long rbp;
+// 	unsigned long rbx;
+// /* These regs are callee-clobbered. Always saved on kernel entry. */
+// 	unsigned long r11;
+// 	unsigned long r10;
+// 	unsigned long r9;
+// 	unsigned long r8;
+// 	unsigned long rax;
+// 	unsigned long rcx;
+// 	unsigned long rdx;
+// 	unsigned long rsi;
+// 	unsigned long rdi;
+// /*
+//  * On syscall entry, this is syscall#. On CPU exception, this is error code.
+//  * On hw interrupt, it's IRQ number:
+//  */
+// 	unsigned long orig_rax;
+// /* Return frame for iretq */
+// 	unsigned long rip;
+// 	unsigned long cs;
+// 	unsigned long eflags;
+// 	unsigned long rsp;
+// 	unsigned long ss;
+// /* top of stack page */
+// };
 
 #endif /* __VMLINUX_H__ */
