d83f87a82d3f280ccdbcd40f0bd022e5816dec15
chriskaliX <chriskali@163.com>
2021-12-31 00:02:05 +0800

    fix

--- agent/collector/ebpf/tracer/tracer.c
+++ agent/collector/ebpf/tracer/tracer.c
@@ -53,6 +53,13 @@ int enter_execve(struct execve_entry_args_t *ctx)
     save_str_arr_to_buf_with_allows(&data, (const char *const *)ctx->envp, 2);
     bpf_probe_read(&(data.submit_p->buf[0]), sizeof(context_t), &data.context);
 
+    // 对于 cwd 的获取实现, tracee 上有一个很好的 issue
+    // https://github.com/aquasecurity/tracee/issues/852
+    // 简单来说目前读取的时候没有办法对 dentry tree & mount point 添加锁, 所以会导致读取的时候可能会出现数据不准确的情况
+    // 在目前的 Hades 中, 我们可以在 execve 中变相的从 envp 中读取(也是不可靠的, 但是能用...)。但是长期来看，应该还是要按照 tracee 的方法来满足其他 hook 点的需求
+    // 另外也提到了 https://github.com/Gui774ume/fsprobe 中的读取方式
+    // TODO: take a look
+
     // satisfy validator by setting buffer bounds
     int size = data.buf_off & ((MAX_PERCPU_BUFSIZE)-1);
     void *output_data = data.submit_p->buf;
