2cf9b249df72e634aafcb7022f78ba2068f45503
chriskaliX <chriskali@163.com>
2022-01-05 00:08:29 +0800

    success with temp buffer...

--- agent/collector/ebpf/include/helpers.h
+++ agent/collector/ebpf/include/helpers.h
@@ -1,23 +1,15 @@
-static __always_inline int has_prefix(char *prefix, char *str, int n)
+static __inline int has_prefix(char *prefix, char *str, int n)
 {
+    int i;
     #pragma unroll
-    for (int i = 0; i < n; prefix++, str++, i++) {
+    for (i = 0; i < n; prefix++, str++, i++) {
         if (!*prefix)
             return 1;
         if (*prefix != *str) {
             return 0;
         }
     }
+
     // prefix is too long
     return 0;
-}
-
-static int has_prefix_new(const char *prefix, const char *str, int n)
-{
-    #pragma unroll
-    for (int i = 0; i < n; i++) {
-        if (prefix[i] != str[i])
-            return 0;
-    }
-    return 1;
 }
