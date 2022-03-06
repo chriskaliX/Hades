65347c8f31651189944ab28068d72ff15480ad37
chriskaliX <chriskaliX@163.com>
2022-03-06 13:20:38 +0800

    add ptrace

--- plugin/driver/eBPF/userspace/parser/ptrace.go
+++ plugin/driver/eBPF/userspace/parser/ptrace.go
@@ -4,30 +4,46 @@ import (
 	"encoding/binary"
 	"hades-ebpf/userspace/cache"
 	"io"
+	"os"
+	"strconv"
 )
 
 // TODO: unfinished
 func Ptrace(buf io.Reader, process *cache.Process) (err error) {
 	var (
 		index    uint8
-		requests int32
-		pid      int32
-		addr     uint32
-		data     uint32
+		requests int64
+		pid      int64
+		addr     uint64
 	)
+	// 0 exe
+	if process.Exe, err = ParseStr(buf); err != nil {
+		return
+	}
+	// 1 request
 	if err = binary.Read(buf, binary.LittleEndian, &index); err != nil {
 		return
 	}
 	if err = binary.Read(buf, binary.LittleEndian, &requests); err != nil {
 		return
 	}
+	// 2 pid
+	if err = binary.Read(buf, binary.LittleEndian, &index); err != nil {
+		return
+	}
 	if err = binary.Read(buf, binary.LittleEndian, &pid); err != nil {
+		return
+	}
+	// 3 addr
+	if err = binary.Read(buf, binary.LittleEndian, &index); err != nil {
 		return
 	}
 	if err = binary.Read(buf, binary.LittleEndian, &addr); err != nil {
 		return
 	}
-	if err = binary.Read(buf, binary.LittleEndian, &data); err != nil {
+	// 4 pid tree
+	if process.PidTree, err = ParsePidTree(buf); err != nil {
+		os.Stderr.WriteString("err:" + err.Error() + "\n")
 		return
 	}
 	return
