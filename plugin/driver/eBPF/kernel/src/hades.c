601715b8d8ab38561ad4971d46089e2952cfaa8c
chriskaliX <chriskaliX@163.com>
2022-03-16 22:41:20 +0800

    add do_init_mod hook to hades_rootkit

--- plugin/driver/eBPF/kernel/src/hades.c
+++ plugin/driver/eBPF/kernel/src/hades.c
@@ -1,6 +1,7 @@
 #include "hades_exec.h"
 #include "hades_net.h"
 #include "hades_privilege.h"
+#include "hades_rootkit.h"
 
 // cat /sys/kernel/debug/kprobes/list to observe the points we've hooked
 // all is from tracee & some from datadog-agent, I do some modification though!!!
