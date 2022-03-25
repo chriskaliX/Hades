d5d99ae84177f3b8ce7e6f5d080c92db84f7c855
chriskaliX <chriskali@163.com>
2022-03-25 18:05:55 +0800

    speed up

---
+++ plugin/driver/eBPF/test/Foo.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.lang.Thread;
+
+public class Foo {
+    public static void main(String[] args) {
+        // String requestMsg = "client xml or json 数据";
+        // String address = "47.102.155.13";
+        // int port = 22;
+        // send(address,port,requestMsg);
+        for (int i = 0 ; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                Runtime.getRuntime().gc();
+                System.out.println("Enforced GC!");
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    /**
+     * Socket 客户端请求
+     *
+     * @param address ip地址
+     * @param port 端口
+     * @param requestMsg 请求内容
+     */
+    public static void send(String address,int port, String requestMsg) {
+
+        try {
+            //创建Socket对象
+            Socket socket=new Socket(address,port);
+            /**
+             * 根据输入输出流和服务端连接
+             * 1）获取一个输出流，向服务端发送信息
+             * 2）将输出流包装成打印流
+             * 3）关闭输出流
+             */
+            OutputStream outputStream=socket.getOutputStream();
+            PrintWriter printWriter=new PrintWriter(outputStream);
+            printWriter.print(requestMsg);
+            printWriter.flush();
+            socket.shutdownOutput();
+
+            //获取一个输入流，接收服务端的信息
+            InputStream inputStream=socket.getInputStream();
+            //包装成字符流，提高效率
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            //缓冲区
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+            StringBuffer sb = new StringBuffer();
+            //临时变量
+            String temp=null;
+            while((temp=bufferedReader.readLine())!=null){
+                sb.append(temp).append("\n");
+            }
+            System.out.println("客户端接收服务端发送信息："+sb.toString());
+
+            //关闭相对应的资源
+            bufferedReader.close();
+            inputStream.close();
+            printWriter.close();
+            outputStream.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ plugin/driver/eBPF/userspace/event/callusermodehelper.go
@@ -0,0 +1,67 @@
+package event
+
+import (
+	"hades-ebpf/userspace/decoder"
+	"strings"
+
+	manager "github.com/ehids/ebpfmanager"
+)
+
+var DefaultCallUsermodeHelper = &CallUsermodeHelper{}
+
+var _ decoder.Event = (*CallUsermodeHelper)(nil)
+
+type CallUsermodeHelper struct {
+	Exe  string `json:"-"`
+	Path string `json:"path"`
+	Argv string `json:"argv"`
+	Envp string `json:"envp"`
+	Wait int32  `json:"wait"`
+}
+
+func (CallUsermodeHelper) ID() uint32 {
+	return 1030
+}
+
+func (CallUsermodeHelper) String() string {
+	return "call_usermodehelper"
+}
+
+func (c *CallUsermodeHelper) GetExe() string {
+	return c.Exe
+}
+
+func (c *CallUsermodeHelper) Parse() (err error) {
+	if c.Path, err = decoder.DefaultDecoder.DecodeString(); err != nil {
+		return
+	}
+	var Argv []string
+	var Envp []string
+	if Argv, err = decoder.DefaultDecoder.DecodeStrArray(); err != nil {
+		return
+	}
+	c.Argv = strings.Join(Argv, " ")
+	if Envp, err = decoder.DefaultDecoder.DecodeStrArray(); err != nil {
+		return
+	}
+	c.Envp = strings.Join(Envp, " ")
+	if err = decoder.DefaultDecoder.DecodeInt32(&c.Wait); err != nil {
+		return
+	}
+	c.Exe, err = decoder.DefaultDecoder.DecodeString()
+	return
+}
+
+func (CallUsermodeHelper) GetProbe() []*manager.Probe {
+	return []*manager.Probe{
+		{
+			Section:          "kprobe/call_usermodehelper",
+			EbpfFuncName:     "kprobe_call_usermodehelper",
+			AttachToFuncName: "call_usermodehelper",
+		},
+	}
+}
+
+func init() {
+	decoder.Regist(DefaultCallUsermodeHelper)
+}
