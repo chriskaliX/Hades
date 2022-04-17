b11975ad95dd7f8ec9cb8dc78980c490dc13ba54
chriskaliX <chriskali@163.com>
2022-04-17 16:39:57 +0800

    update for CO-RE obj file compile passed

--- plugin/driver/eBPF/kernel/include/define.h
+++ plugin/driver/eBPF/kernel/include/define.h
@@ -43,13 +43,14 @@
 #define EXECVE_GET_SOCK_PID_LIMIT 4
 
 /* ========== MAP MICRO DEFINATION ========== */
-#define BPF_MAP(_name, _type, _key_type, _value_type, _max_entries) \
-    struct bpf_map_def SEC("maps") _name = {                        \
-        .type = _type,                                              \
-        .key_size = sizeof(_key_type),                              \
-        .value_size = sizeof(_value_type),                          \
-        .max_entries = _max_entries,                                \
-    };
+// update since bpf_map_def is marked as deprecated
+#define BPF_MAP(_name, _type, _key_type, _value_type, _max_entries)     \
+    struct {                                                            \
+        __uint(type, _type);                                            \
+        __uint(max_entries, _max_entries);                              \
+        __type(key, _key_type);                                         \
+        __type(value, _value_type);                                     \
+    } _name SEC(".maps");
 /* BPF MAP DEFINATION MICROS, MODIFIED WITH MAX_ENTRIES */
 #define BPF_HASH(_name, _key_type, _value_type, _max_entries) \
     BPF_MAP(_name, BPF_MAP_TYPE_HASH, _key_type, _value_type, _max_entries)
