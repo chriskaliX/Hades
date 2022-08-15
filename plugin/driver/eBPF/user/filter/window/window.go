b90fafaa61745d2de27e6e549924710c80256fdc
chriskaliX <chriskali@163.com>
2022-08-15 10:46:49 +0800

    Driver-eBPF: remove invalid docs

--- plugin/driver/eBPF/user/filter/window/window.go
+++ plugin/driver/eBPF/user/filter/window/window.go
@@ -1,11 +1,3 @@
-/*
- * @Author: chriskaliX chriskali@163.com
- * @Date: 2022-08-07 00:07:45
- * @LastEditors: chriskaliX chriskali@163.com
- * @LastEditTime: 2022-08-15 10:38:53
- * @FilePath: /Hades/plugin/driver/eBPF/user/filter/window/window.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 package window
 
 import (
