39db6b833fe5e84298bda99f67799e35a63d3c8d
chriskaliX <chriskali@163.com>
2022-09-27 22:53:21 +0800

    collector: multi bug fix

---
+++ plugin/collector/cache/ticker.go
@@ -0,0 +1,30 @@
+package cache
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+var GTicker = &TickerClock{}
+
+type TickerClock struct {
+	cachetime atomic.Value
+}
+
+func (t *TickerClock) Now() time.Time {
+	return t.cachetime.Load().(time.Time)
+}
+
+func init() {
+	GTicker.cachetime.Store(time.Now())
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				GTicker.cachetime.Store(time.Now())
+			}
+		}
+	}()
+}
