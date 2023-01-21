0737247d5827483a5dbe2670498e2d5ab6dcf581
chriskaliX <chriskali@163.com>
2023-01-21 23:47:15 +0800

    remove cgroupv2 in sysvinit, since not possible

--- agent/deploy/control/cmd/start.go
+++ agent/deploy/control/cmd/start.go
@@ -5,14 +5,12 @@ Copyright © 2022 chriskali
 package cmd
 
 import (
-	"errors"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"syscall"
 
 	"github.com/containerd/cgroups"
-	"github.com/containerd/cgroups/v3/cgroup2"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -52,64 +50,45 @@ func sysvinitStart() error {
 	// if cgroup do not work as expected, quit the agent
 	// should cgroup2 being covered? MAY EVENT NOT IN SYSVINIT
 	// cgroup here
-	var cg2 bool
-	switch cgroups.Mode() {
-	case cgroups.Unavailable:
-		return errors.New("cgroup unavailiable")
-	case cgroups.Hybrid, cgroups.Unified:
-		// Hybrid: cgroup v1 and v2, let's use v2
-		// Unified: v2
-		cg2 = true
-	case cgroups.Legacy:
-		// cgroup v1
-		cg2 = false
-	}
+	// cgroup v2 need kernel version over 5.8, PRETTY SURE no sysvinit available
+
 	// resource limitation
 	quota := int64(10000)
-	period := uint64(100000)
+	// period := uint64(100000)
 	memory := int64(1024 * 1024 * 250)
-	if cg2 {
-		res := cgroup2.Resources{
-			CPU: &cgroup2.CPU{
-				Max: cgroup2.NewCPUMax(&quota, &period),
-			},
-			Memory: &cgroup2.Memory{
-				Max: &memory,
-			},
-		}
-		group := "/" + serviceName + ".slice"
-		// load before we start
-		if manager, err := cgroup2.Load(group, cgroup2.WithMountpoint(cgroupPath)); err == nil {
-			return manager.AddProc(uint64(cmd.Process.Pid))
-		}
-		// load failed, re-create the cgroup v2 by the manager
-		manager, err := cgroup2.NewManager(cgroupPath, group, &res)
-		if err != nil {
-			return err
-		}
-		return manager.AddProc(uint64(cmd.Process.Pid))
-	} else {
-		// pre check the cgroup
-		cg, err := cgroups.Load(V1, cgroups.StaticPath(cgroupPath))
-		if err == nil {
-			return cg.AddProc(uint64(cmd.Process.Pid))
-		}
-		// not exist, new a cgroup
-		// Cpu 10%, Mem 250M
-		cg, err = cgroups.New(V1,
-			cgroups.StaticPath(cgroupPath),
-			&specs.LinuxResources{
-				CPU: &specs.LinuxCPU{
-					Quota: &quota,
-				},
-				Memory: &specs.LinuxMemory{
-					Limit: &memory,
-				},
-			})
-		if err == nil {
-			return cg.AddProc(uint64(cmd.Process.Pid))
-		}
+	// pre check the cgroup
+	cg, err := cgroups.Load(V1, cgroups.StaticPath(cgroupPath))
+	if err == nil {
+		return cg.AddProc(uint64(cmd.Process.Pid))
 	}
+	// not exist, new a cgroup
+	// Cpu 10%, Mem 250M
+	cg, err = cgroups.New(V1,
+		cgroups.StaticPath(cgroupPath),
+		&specs.LinuxResources{
+			CPU: &specs.LinuxCPU{
+				Quota: &quota,
+			},
+			Memory: &specs.LinuxMemory{
+				Limit: &memory,
+			},
+		})
+	if err == nil {
+		return cg.AddProc(uint64(cmd.Process.Pid))
+	}
+	// WARNING: MOUNTING NOT READY, need to mount
+	// cat /proc/self/mountinfo|grep -q 'cgroup .* rw,.*\bmemory\b'
+	// if [ $? -ne 0 ];then
+	//     info "memory cgroup is umounted, trying mounting"
+	//     expect "mkdir -p ${root_dir}/cgroup/memory"
+	//     expect "mount -t cgroup -o memory cgroup ${root_dir}/cgroup/memory"
+	// fi
+	// cat /proc/self/mountinfo|grep -q 'cgroup .* rw,.*\bcpu\b'
+	// if [ $? -ne 0 ];then
+	//     info "cpu cgroup is umounted, trying mounting"
+	//     expect "mkdir -p ${root_dir}/cgroup/cpu"
+	//     expect "mount -t cgroup -o cpu cgroup ${root_dir}/cgroup/cpu"
+	// fi
 	return err
 }
 
