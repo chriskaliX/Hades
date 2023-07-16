b88752d6d08e771e501b07aef8aa737eba37f960
chriskaliX <chriskali@163.com>
2023-07-16 19:05:57 +0800

    edriver: patch in execveat

--- plugins/edriver/bpf/include/hades_exec.h
+++ plugins/edriver/bpf/include/hades_exec.h
@@ -92,7 +92,6 @@ int sys_exit_execve(void *ctx)
     struct fs_struct *file = get_task_fs(data.task);
     if (file == NULL)
         goto delete;
-    // try to patch the stack
     void *file_path = get_path_str_simple(GET_FIELD_ADDR(file->pwd));
     save_str_to_buf(&data, file_path, 1);
     void *ttyname = get_task_tty_str(data.task);
@@ -166,7 +165,7 @@ int sys_exit_execveat(void *ctx)
     struct fs_struct *file = get_task_fs(data.task);
     if (file == NULL)
         goto delete;
-    void *file_path = get_path_str(GET_FIELD_ADDR(file->pwd));
+    void *file_path = get_path_str_simple(GET_FIELD_ADDR(file->pwd));
     save_str_to_buf(&data, file_path, 1);
     void *ttyname = get_task_tty_str(data.task);
     save_str_to_buf(&data, ttyname, 2);
