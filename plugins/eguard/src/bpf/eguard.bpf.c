06114984ef9a5b9feb550944e67791c7cc3fbbb3
chriskaliX <chriskali@163.com>
2023-07-29 01:48:58 +0800

    eguard: support ingress ACL

--- plugins/eguard/src/bpf/eguard.bpf.c
+++ plugins/eguard/src/bpf/eguard.bpf.c
@@ -3,7 +3,7 @@
 #include "../../../libs/core/vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
-#include "egress.h"
+#include "tc.h"
 // #include "xdp.h"
 
 // Egress-based packet drop
